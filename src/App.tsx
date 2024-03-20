57ec4759088f2c88ee2fe89b0f7637325813e7d9
vika200611700 <27539313-vika200611700@users.noreply.replit.com>
2024-03-20 12:56:49 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PluginFileCover_1 from './components/PluginFileCover_1';
+
+function App() {
+  return (
+    <>
+      <PluginFileCover_1 />
+    </>
+  );
+}
+
+export default App;
